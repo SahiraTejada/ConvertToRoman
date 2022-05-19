5015ebada100c6f99e1f0961d116e415153fd5bd
SahiraTejada <81046432+SahiraTejada@users.noreply.github.com>
2022-05-19 11:40:02 -0400

    Add existing file

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hola mundo")
+
+texto = "Riop"
+nombre = "victor"
+year = 2020
+
+print(f"{year}{nombre}")
+
+
+    ALTURA = int(input("Cual es la altura: "))
+    if ALTURA>= 180:
+        print("eres alto")
+    else:
+        print("eres bajo")
+
+def mostraraltura():
+
